8a8c3d9e8e31f7375ed526c6a66e136e6fdd98ce
sonir <isana@sonir.jp>
2015-09-06 05:47:51 +0900

    1st

---
+++ init.sh
@@ -0,0 +1,6 @@
+echo "# 1509_skShare" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/sonir/1509_skShare.git
+git push -u origin master
